fe57581d891457758994e80e3c01ea42111a6b5d
AlievMagomed <steam.30mb1@gmail.com>
2017-08-11 18:12:30 +0300

    Download instumet added.
    File is being uploaded to amazon s3 service and then a link for downloading is given.

---
+++ download.py
@@ -0,0 +1,31 @@
+import boto3
+from database import Data
+import json
+
+db = Data()
+coursor = db.get_tickers()
+res_dict = {}
+first = True
+
+for ticker in coursor:
+    if first:
+        _from = ticker['time']
+        first = False
+    time = ticker['time']
+    last = time
+    ticker.pop('time')
+    ticker.pop('_id')
+    res_dict[time] = ticker
+
+data_name = _from[:10] + ' - ' + last[:10] + '.json'
+tmp = 'tickers/' + data_name
+with open(tmp, 'w') as outfile:
+    json.dump(res_dict, outfile)
+
+client = boto3.client('s3', region_name='eu-central-1')
+bucket_name = 'exchangestat'
+client.upload_file(tmp, bucket_name, data_name)
+
+
+presigned_url = client.generate_presigned_url('get_object', Params = {'Bucket': 'exchangestat', 'Key': data_name})
+print (presigned_url)
